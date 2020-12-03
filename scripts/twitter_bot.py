b9fe068c62191d1c42ec1658e6b7cd5a7557e047
Lukas Lamm <lamm@ifam.rwth-aachen.de>
2020-12-03 16:42:29 +0100

    Refactor twitter_bot to oop

--- scripts/twitter_bot.py
+++ scripts/twitter_bot.py
@@ -6,66 +6,96 @@ from scraper import scraper
 from export_data import export_data
 
 
-def connect_twitter_api(credential_file):
-    # Connect to twitter API unsig credentials from json file
+class twitter_bot():
     
-    # Load credentials
-    with open(credential_file) as file:
-        credentials = json.load(file)
-
-    # Establish connection to api
-    auth = tweepy.OAuthHandler(credentials["API-key"], credentials["API-key-secret"])
-    auth.set_access_token(credentials["Access-token"], credentials["Access-token-secret"])
-    api = tweepy.API(auth)
-
-    return api
+    def __init__(self, credential_source):
+        self.credential_source = credential_source
+        self.is_connected = False
 
 
-def find_tweets_by_pattern(tweets, pattern):
-    # Find tweets be predefined regEx pattern
-    matching_tweets = []
-    regex = re.compile(pattern)
+    def connect_to_twitter(self):
+        if not self.is_connected:
+            # Load credentials
+            with open(self.credential_source) as file:
+                credentials = json.load(file)
 
-    for tweet in tweets:
-            match = regex.search(tweet.full_text)
-            if(match):
+            # Establish connection to api
+            auth = tweepy.OAuthHandler(credentials["API-key"], credentials["API-key-secret"])
+            auth.set_access_token(credentials["Access-token"], credentials["Access-token-secret"])
+            self.api = tweepy.API(auth)
+            self.is_connected = True
+        else:
+            raise RuntimeError('Can not connect to twitter! Bot is already connected!')
+
+
+    def get_user_timeline(self, user_ID, count=100, include_rts=False, tweet_mode='extended'):
+        if self.is_connected:
+            return self.api.user_timeline(screen_name=user_ID,
+                                        count=count,
+                                        include_rts = include_rts,
+                                        tweet_mode = tweet_mode)
+        else:
+            raise RuntimeError('Can not get timeline! Bot is not connected to API!')
+
+
+    def reply_to_tweet(self, message, tweet_id):
+        if self.is_connected:
+            self.api.update_status(status=message, in_reply_to_status_id=tweet_id)
+        else:
+            raise RuntimeError('Can not reply to tweet! Bot is not connected to API!')
+
+
+    def reply_to_tweet_with_media(self, filename, tweet_id, message=''):
+        if self.is_connected:
+            self.api.update_with_media(filename, status=message, in_reply_to_status_id=tweet_id)
+        else:
+            raise RuntimeError('Can not reply to tweet! Bot is not connected to API!')
+
+    
+    @staticmethod
+    def find_tweets_by_pattern(tweets, pattern):
+        # Find tweets be predefined regEx pattern
+        matching_tweets = []
+        regex = re.compile(pattern)
+
+        for tweet in tweets:
+                match = regex.search(tweet.full_text)
+                if(match):
+                    matching_tweets.append(tweet)
+
+        return matching_tweets
+
+
+    @staticmethod
+    def find_tweets_by_date(tweets, date):
+        # Find tweets postet on a certain date
+        matching_tweets = []
+
+        for tweet in tweets:
+            if (date == datetime.date(tweet.created_at)):
                 matching_tweets.append(tweet)
 
-    return matching_tweets
-
-
-def find_tweets_by_date(tweets, date):
-    # Find tweets postet on a certain date
-    matching_tweets = []
-
-    for tweet in tweets:
-        if (date == datetime.date(tweet.created_at)):
-            matching_tweets.append(tweet)
-
-    return matching_tweets
+        return matching_tweets
 
 
 if __name__ == "__main__":
 
     # Connect to API
     CREDFILE = "./twitter_credentials/twitter_credentials.json"
-    api = connect_twitter_api(CREDFILE)    
+    bot = twitter_bot(CREDFILE)  
+    bot.connect_to_twitter()
     
     # Get tweets of PresseamtAachen
     userID = 'Mechaniac2'
-    tweets = api.user_timeline(screen_name=userID, 
-                           count=100,
-                           include_rts = False,
-                           tweet_mode = 'extended'
-                           )
+    tweets = bot.get_user_timeline(userID)
 
     # Get current date
     curr_date = date.today()
     
     # Analyse tweets
     pattern = "(Corona-Virus|Fälle|Sieben-Tage-Inzidenz)"
-    matched_tweets = find_tweets_by_pattern(tweets, pattern)
-    matched_tweets = find_tweets_by_date(matched_tweets, curr_date)
+    matched_tweets = bot.find_tweets_by_pattern(tweets, pattern)
+    matched_tweets = bot.find_tweets_by_date(matched_tweets, curr_date)
 
     if matched_tweets:
         print('Found matching tweets! Start replying...')
