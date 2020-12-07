12a2a2e0566407da41df80407136120d5572168f
Lukas <lamm.lukas@gmail.com>
2020-12-07 22:30:41 +0100

    Modified main file to new scraper oop

--- scripts/main.py
+++ scripts/main.py
@@ -1,9 +1,14 @@
 from scraper import scraper
 from export_data import export_data
+from scraper import covidScraper
 
 if __name__ == "__main__":
+
+    URL = 'http://aachen.de/DE/stadt_buerger/notfall_informationen/corona/aktuelles/index.html'
+
     # Run the scraper
-    scraper()
+    scraper = covidScraper(URL, '../data/data.db')
+    scraper.scrape()
 
     # Export data from db
     export_data()
